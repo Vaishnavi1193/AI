5e12b6d925061326feba3df0229ed79a4966f081
Vaishnavi1193 <107098851+Vaishnavi1193@users.noreply.github.com>
2023-11-04 13:14:39 +0530

    Update 26.fruit color.pl

--- 26.fruit color.pl
+++ 26.fruit color.pl
@@ -5,3 +5,14 @@ colour(apple, green).
 colour(orange, orange).
 colour(X, unknown).
 OUTPUT:
+?- colour(A,B).
+A = cherry,
+B = red ;
+A = banana,
+B = yellow ;
+A = apple,
+B = red ;
+A = apple,
+B = green ;
+A = B, B = orange ;
+B = unknown.
